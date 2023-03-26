0d7161501f798f700ac8dc0c690e50193b401f16
rekhakonjarla <konjarlarekha@gmail.com>
2023-03-26 14:10:27 +0530

    demo

---
+++ Java_Tag_Content_Extractor.java
@@ -0,0 +1,33 @@
+25. JAVA TAG CONTENT EXTRACTOR
+
+SOLUTION:-
+
+import java.io.*;
+import java.util.*;
+import java.text.*;
+import java.math.*;
+import java.util.regex.*;
+
+public class Solution{
+    
+   public static void main(String[] args){
+      Scanner in = new Scanner(System.in);
+      int t = Integer.parseInt(in.nextLine());
+      
+      while(t-->0){
+          String line = in.nextLine();
+         
+          Matcher m = Pattern.compile("<(.+)>(([^<>]+))</\\1>").matcher(line);
+          
+          if (!m.find()) {
+              System.out.println("None");
+              continue;
+          }
+          
+          m.reset();
+          while (m.find()){
+              System.out.println(m.group(2));
+          }
+       }
+    }
+ }
